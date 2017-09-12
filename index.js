440f3ca3987cca9edfc15a687322dbae40bd2ad8
abetomo <abe@enzou.tokyo>
2017-09-12 13:30:28 +0900

    Fix not to use `path`

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 'use srict'
-const path = require('path')
-module.exports = require(path.join(__dirname, 'lib', 'very_simple_queue'))
-module.exports.SQS = require(path.join(__dirname, 'lib', 'very_simple_queue_like_sqs'))
+module.exports = require('./lib/very_simple_queue')
+module.exports.SQS = require('./lib/very_simple_queue_like_sqs')
